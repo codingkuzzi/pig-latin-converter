6c864cd5a4bdf2898cfad480fb9db9f44f93710e
Anna Kuznetsova and Joe Kramer <anna@>
2017-06-07 13:43:05 -0700

    add regex to back end logic

--- js/scripts.js
+++ js/scripts.js
@@ -2,40 +2,48 @@ $(document).ready(function() {
   $("form#pigLatin").submit(function(event) {
     event.preventDefault();
     var sentence = $("#sentence").val();
-    if(isNumeric(sentence)) { alert('number'); }
+    if(sentence.match(/\d/)) { alert('number'); }
     var result = translate(sentence);
     $("#result").text(result);
   });
 });
 
 //BACK END
-var translate = function(word) {
+var translate = function(sentence) {
+  var finalSentence = [];
+  var wordArray = sentence.split(" ");
+  wordArray.forEach(function(word) {
+    var letterArray = word.split("");
+    console.log(letterArray[0]);
+    if (letterArray[0].match(/[aeiou]/i)) {
+      console.log("here");
+      var newWord = letterArray.concat("way");
+      var newWord1 = newWord.join('');
+      finalSentence.push(newWord1);
+      return;
+    }
 
-  var letterArray = word.split("");
-  if (letterArray[0] === "a" || letterArray[0] === "e" || letterArray[0] === "i" || letterArray[0] === "o" || letterArray[0] === "u") {
-    var newWord = letterArray.concat("way");
-    return newWord.join('');
-  }
+    for (var i = 0; i < letterArray.length; i++) {
 
-  for (var i = 0; i < letterArray.length; i++) {
-
-    if(letterArray[i] === "q") {
-      if(letterArray[i+1] === "u") {
-        var removed = letterArray.splice(0, i + 2);
+      if(letterArray[i] === "q") {
+        if(letterArray[i+1] === "u") {
+          var removed = letterArray.splice(0, i + 2);
+          var firstHalf = letterArray.join('');
+          var secondHalf = removed.join('');
+          var finalWord = firstHalf.concat(secondHalf);
+          finalSentence.push(finalWord);
+          return;
+        }
+      } else if (letterArray[i].match(/[aeiou]/i)) {
+        var removed = letterArray.splice(0, i);
         var firstHalf = letterArray.join('');
         var secondHalf = removed.join('');
-        return firstHalf.concat(secondHalf);
+        var finalWord = firstHalf.concat(secondHalf);
+        var finalWord2 = finalWord.concat("ay");
+        finalSentence.push(finalWord2);
+        return;
       }
-    } else if (letterArray[i] === "a" || letterArray[i] === "e" || letterArray[i] === "i" || letterArray[i] === "o" || letterArray[i] === "u") {
-      var removed = letterArray.splice(0, i);
-      var firstHalf = letterArray.join('');
-      var secondHalf = removed.join('');
-      var finalWord = firstHalf.concat(secondHalf);
-      return finalWord.concat("ay");
     }
-  }
+  });
+  return finalSentence.join(' ');
 };
-
-function isNumeric(n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-}
